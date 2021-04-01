855ae0afff16ac69dee5d56898cab417f58c06ff
BarashkovHSE <63236438+BarashkovHSE@users.noreply.github.com>
2021-04-01 15:57:48 +0300

    Update model.py

--- model.py
+++ model.py
@@ -2,18 +2,15 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
-
-
 # from data_loader import get_loader
 
 
 class ConditionalInstanceNormalisation(nn.Module):
     """CIN Block."""
-
     def __init__(self, in_channel, n_speakers):
         super(ConditionalInstanceNormalisation, self).__init__()
 
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
         self.dim_in = in_channel
         self.gamma = nn.Linear(n_speakers, in_channel)
@@ -34,14 +31,12 @@ class ConditionalInstanceNormalisation(nn.Module):
 
         return h
 
-
 class ResidualBlock(nn.Module):
     """Residual Block with instance normalization."""
-
     def __init__(self, in_channel, out_channel, n_speakers):
         super(ResidualBlock, self).__init__()
         self.conv_1 = nn.Conv1d(in_channel, out_channel, kernel_size=3, stride=1, padding=1, bias=False)
-        self.cin_1 = ConditionalInstanceNormalisation(out_channel, n_speakers * 2)
+        self.cin_1 = ConditionalInstanceNormalisation(out_channel, n_speakers*2)
         self.relu_1 = nn.GLU(dim=1)
 
     def forward(self, x, c):
@@ -51,69 +46,61 @@ class ResidualBlock(nn.Module):
 
         return x
 
-
 class Down2d_initial(nn.Module):
     """docstring for Down2d."""
-
-    def __init__(self, in_channel, out_channel, kernel, stride, padding):
+    def __init__(self, in_channel ,out_channel, kernel, stride, padding):
         super(Down2d_initial, self).__init__()
 
         self.c1 = nn.Conv2d(in_channel, out_channel, kernel_size=kernel, stride=stride, padding=padding)
         self.glu = nn.GLU(dim=1)
-
+    
     def forward(self, x):
         x1 = self.c1(x)
         x1 = self.glu(x1)
 
         return x1
-
-
+         
 class Down2d(nn.Module):
     """docstring for Down2d."""
-
-    def __init__(self, in_channel, out_channel, kernel, stride, padding):
+    def __init__(self, in_channel ,out_channel, kernel, stride, padding):
         super(Down2d, self).__init__()
 
         self.c1 = nn.Conv2d(in_channel, out_channel, kernel_size=kernel, stride=stride, padding=padding)
         self.n1 = nn.InstanceNorm2d(out_channel, affine=True, track_running_stats=True)
         self.glu = nn.GLU(dim=1)
-
+    
     def forward(self, x):
         x1 = self.c1(x)
         x1 = self.n1(x1)
         x1 = self.glu(x1)
-
-        return x1
-
-
+        
+        return x1   
+        
 class Up2d(nn.Module):
     """docstring for Up2d."""
-
-    def __init__(self, in_channel, out_channel, kernel, stride, padding):
+    def __init__(self, in_channel ,out_channel, kernel, stride, padding):
         super(Up2d, self).__init__()
         self.c1 = nn.ConvTranspose2d(in_channel, out_channel, kernel_size=kernel, stride=stride, padding=padding)
         self.n1 = nn.InstanceNorm2d(out_channel, affine=True, track_running_stats=True)
         self.glu = nn.GLU(dim=1)
-
+         
     def forward(self, x):
         x1 = self.c1(x)
         x1 = self.n1(x1)
         x1 = self.glu(x1)
-
+        
         return x1
 
 
 class Generator(nn.Module):
     """Generator network."""
-
-    #    def __init__(self, conv_dim=64, num_speakers=10, repeat_num=6):
-    def __init__(self, conv_dim=64, num_speakers=70,
-                 repeat_num=6):  # **************************************
+#    def __init__(self, conv_dim=64, num_speakers=10, repeat_num=6): 
+    def __init__(self, conv_dim=64, num_speakers=70,repeat_num=6):#**************************************                                                       
         super(Generator, self).__init__()
         self.downsample = nn.Sequential(
-            Down2d_initial(1, 128, (3, 9), (1, 1), (1, 4)),
-            Down2d(64, 256, (4, 8), (2, 2), (1, 3)),
-            Down2d(128, 512, (4, 8), (2, 2), (1, 3))
+            Down2d_initial(1, 128, (3,9), (1,1), (1,4)),
+            Down2d(64, 256, (4,8), (2,2), (1,3)),
+            Down2d(128, 512, (4,8), (2,2), (1,3))
         )
 
         # Down-conversion layers.
@@ -126,6 +113,7 @@ class Generator(nn.Module):
                       bias=False),
             nn.InstanceNorm1d(num_features=256, affine=True)
         )
+
 
         # Bottleneck layers.
         self.residual_1 = ResidualBlock(in_channel=256, out_channel=512, n_speakers=num_speakers)
@@ -146,17 +134,20 @@ class Generator(nn.Module):
                                        padding=0,
                                        bias=False)
 
-        self.up1 = Up2d(256, 256, (4, 4), (2, 2), (1, 1))
-        self.up2 = Up2d(128, 128, (4, 4), (2, 2), (1, 1))
+        self.up1 = Up2d(256, 256, (4,4), (2,2), (1,1))
+        self.up2 = Up2d(128, 128, (4,4), (2,2), (1,1))
+
 
         self.deconv = nn.Conv2d(in_channels=64, out_channels=1, kernel_size=7, stride=1, padding=3, bias=False)
 
-    def forward(self, x, con1, con2):
-        c = torch.cat((con1, con2), dim=1)
+    def forward(self, x, con1, con2):	
+
+        c = torch.cat((con1, con2), dim=1)					
         width_size = x.size(3)
-        x = self.downsample(x)
+        x = self.downsample(x)	
         x = x.contiguous().view(-1, 2304, width_size // 4)
-        x = self.down_conversion(x)
+        x = self.down_conversion(x)					 
+        
 
         x = self.residual_1(x, c)
         x = self.residual_2(x, c)
@@ -170,11 +161,11 @@ class Generator(nn.Module):
 
         x = self.up_conversion(x)
         x = x.view(-1, 256, 9, width_size // 4)
-
-        x = self.up1(x)
-        x = self.up2(x)
-        x = self.deconv(x)
-
+                               
+        x = self.up1(x) 											
+        x = self.up2(x)										 
+        x = self.deconv(x)						 
+  
         return x
 
 
@@ -185,19 +176,21 @@ class Discriminator(nn.Module):
         super(Discriminator, self).__init__()
 
         self.downsample = nn.Sequential(
-            Down2d_initial(1, 128, (3, 9), (1, 1), (1, 4)),
-            Down2d(64, 256, (3, 3), (2, 2), (1, 1)),
-            Down2d(128, 512, (3, 3), (2, 2), (1, 1)),
-            Down2d(256, 1024, (3, 3), (2, 2), (1, 1)),
-            Down2d(512, 1024, (1, 5), (1, 1), (0, 2))
+            Down2d_initial(1, 128, (3,9), (1,1), (1,4)),
+            Down2d(64, 256, (3,3), (2,2), (1,1)),
+            Down2d(128, 512, (3,3), (2,2), (1,1)),   
+            Down2d(256, 1024, (3,3), (2,2), (1,1)),  
+            Down2d(512, 1024, (1,5), (1,1), (0,2)) 
         )
-
+        
         self.fc = nn.Linear(in_features=512, out_features=1)
-        self.proj = nn.Linear(num_speakers * 2, 512)
+        self.proj = nn.Linear(num_speakers*2, 512)
+
 
     def forward(self, x, con1, con2):
+        
         c = torch.cat((con1, con2), dim=1)
-        # c = con1
+        #c = con1
         x = self.downsample(x)
 
         h = torch.sum(x, dim=(2, 3))
@@ -209,3 +202,22 @@ class Discriminator(nn.Module):
         x += torch.sum(p * h, dim=1, keepdim=True)
 
         return x
+        
+
+
+if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    train_loader = get_loader('/scratch/sxliu/data_exp/VCTK-Corpus-22.05k/mc/train', 16, 'train', num_workers=1)
+    data_iter = iter(train_loader)
+    G = Generator().to(device)
+    D = Discriminator().to(device)
+    for i in range(10):
+        mc_real, spk_label_org, acc_label_org, spk_acc_c_org = next(data_iter)
+        mc_real.unsqueeze_(1) # (B, D, T) -> (B, 1, D, T) for conv2d
+        mc_real = mc_real.to(device)                         # Input mc.
+        spk_label_org = spk_label_org.to(device)             # Original spk labels.
+        acc_label_org = acc_label_org.to(device)             # Original acc labels.
+        spk_acc_c_org = spk_acc_c_org.to(device)             # Original spk acc conditioning.
+        mc_fake = G(mc_real, spk_acc_c_org)
+        print(mc_fake.size())
+        out_src, out_cls_spks, out_cls_emos = D(mc_fake)
